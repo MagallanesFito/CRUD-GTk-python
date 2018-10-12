664d263a17a619d1a1a55770ca243034e816b548
Manuel Manteiga Matanza <manuel.manteiga@udc.es>
2018-10-13 00:17:02 +0200

    Error en Mock eliminado

--- src/Mock.py
+++ src/Mock.py
@@ -1,5 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, Atk
+
 class Mock:
 	def __init__(self):
-		self.list = Gtk.ListStore()
-		viewer.append(["Manuel","Adolfo","Test","Inicial"])
-		viewer.append(["Manuel","Adolfo","Test","Inicial"])
+		list = Gtk.ListStore(str,str,str,str)
+		list.append(["Manuel","Adolfo","Test","Inicial"])
+		list.append(["Manuel","Adolfo","Test","Inicial"])
+		self.list = list
