777c5e7631194c3efb1c1eaffa7bfd37d231465a
Manuel Manteiga Matanza <manuel.manteiga@udc.es>
2018-10-12 20:47:27 +0200

    Main

--- src/Main.py
+++ src/Main.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import Controller
+import View
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Atk
+
+if __name__ == '__main__':
+    import signal
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    controller = Controller.Controller(View.View())
+    Gtk.main()
 
