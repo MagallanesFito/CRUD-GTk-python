446681258a3229cfc6994fa0d5e84c927b4d0826
Manuel Manteiga Matanza <manuel.manteiga@udc.es>
2018-10-12 23:32:35 +0200

    View con bugs y base de Controller

--- src/Controller.py
+++ src/Controller.py
@@ -5,3 +5,20 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Atk
 
+class Controller:
+
+	def __init__(self, view):
+		self.view = view
+		view.connect(self)
+	
+	def onAddButtonClicked(self, w):
+		print("Not implemented")
+
+	def onModifyButtonClicked(self, w):
+		print("Not implemented")
+
+	def onRemoveButtonClicked(self, w):
+		print("Not implemented")
+
+	def onEntrySelectedChanged(self, selection):
+		print("Not implemented")
