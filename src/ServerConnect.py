5ce4744f512bfdd903ced71c1f004d2ddb4a0f99
Manuel Manteiga Matanza <manuel.manteiga@udc.es>
2018-10-25 22:08:22 +0200

    Added ServerConnect for server communication

---
+++ src/ServerConnect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, Atk
+
+class ServerConnect:
+	def addEntry(self, e):
+		return
+	def deleteEntry(self,entry):
+		return
+	def modifyEntry(self, befentry, aftentry):
+		return
+	def get_all(self):
+		return
